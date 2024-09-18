ad7527c6132478d6a3bb71eee42f8ebcb74c925f
Javier Gongora <42308135+javigong@users.noreply.github.com>
2024-09-18 16:16:33 -0700

    Add product sanity type

---
+++ sanity/schemaTypes/productType.ts
@@ -0,0 +1,35 @@
+import {defineField, defineType} from 'sanity'
+
+export const productType = defineType({
+  name: 'product',
+  type: 'document',
+  title: 'Product',
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+      title: 'Product Name',
+    }),
+    defineField({
+      name: 'images',
+      type: 'array',
+      of: [{type: 'image'}],
+      title: 'Product Images',
+    }),
+    defineField({
+      name: 'description',
+      type: 'text',
+      title: 'Product Description',
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      title: 'Product Slug',
+    }),
+    defineField({
+      name: 'price',
+      type: 'number',
+      title: 'Product Price',
+    }),
+  ],
+})
