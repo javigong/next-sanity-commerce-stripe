5db71db0c6d5f99e3ffa12da58d27646d09fbd7f
Javier Gongora <42308135+javigong@users.noreply.github.com>
2024-09-19 10:32:47 -0700

    Add Product Page and Image Gallery

---
+++ app/components/ImageGallery.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import Image from "next/image";
+import { useState } from "react";
+import { urlFor } from "../lib/sanity";
+
+interface ImageGalleryProps {
+  images: SanityImageSource[];
+}
+
+export default function ImageGallery({ images }: ImageGalleryProps) {
+  const [bigImage, setBigImage] = useState(images[0]);
+
+  const handleSmallImageClick = (image: SanityImageSource) => {
+    setBigImage(image);
+  };
+
+  return (
+    <div className="grid gap-4 lg:grid-cols-5">
+      <div className="order-last flex gap-4 lg:order-none lg:flex-col">
+        {images.map((image: SanityImageSource, idx) => (
+          <div key={idx} className="overflow-hidden rounded-lg bg-gray-100">
+            <Image
+              src={urlFor(image).url()}
+              alt="photo"
+              width={200}
+              height={200}
+              className="h-full w-full object-cover object-center cursor-pointer"
+              onClick={() => handleSmallImageClick(image)}
+            />
+          </div>
+        ))}
+      </div>
+
+      <div className="relative overflow-hidden rounded-lg bg-gray-100 lg:col-span-4">
+        <Image
+          src={urlFor(bigImage).url()}
+          alt="Photo"
+          width={500}
+          height={500}
+          className="h-full w-ful object-cover object-center"
+        />
+      </div>
+    </div>
+  );
+}
--- app/lib/interface.ts
+++ app/lib/interface.ts
@@ -1,8 +1,19 @@
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
 export interface simplifiedProduct {
   _id: string;
   imageUrl: string;
   price: number;
   name: string;
+  slug: string;
+  categoryName: string;
+}
+export interface fullProduct {
+  _id: string;
+  images: SanityImageSource[];
+  price: number;
+  name: string;
+  description: string;
   slug: string;
   categoryName: string;
 }
