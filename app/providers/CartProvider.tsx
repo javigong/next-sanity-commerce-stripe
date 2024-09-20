596c20fc8578c2bd1981a0f84c2e22dbf2daeea6
Javier Gongora <42308135+javigong@users.noreply.github.com>
2024-09-19 18:21:05 -0700

    Add stripe success and error pages

--- app/providers/CartProvider.tsx
+++ app/providers/CartProvider.tsx
@@ -1,18 +1,26 @@
 "use client";
 
-import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 import { CartProvider as ShoppingCartProvider } from "use-shopping-cart";
 
 export default function CartProvider({ children }: { children: ReactNode }) {
+  const env = process.env.NODE_ENV;
 
   return (
     <ShoppingCartProvider
       mode="payment"
       cartMode="client-only"
       stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
-      successUrl={"http://localhost:3000/success"}
-      cancelUrl={"http://localhost:3000/error"}
+      successUrl={
+        env === "production"
+          ? `${process.env.VERCEL_URL}/stripe/success`
+          : "http://localhost:3000/stripe/success"
+      }
+      cancelUrl={
+        env === "production"
+          ? `${process.env.VERCEL_URL}/stripe/error`
+          : "http://localhost:3000/stripe/error"
+      }
       currency="USD"
       billingAddressCollection={false}
       shouldPersist={true}
